9cef80e7de3a527ee96092d2fc97904258d4b074
Maria Ensari <149113366+marotr@users.noreply.github.com>
2024-04-13 15:54:03 +0300

    finished hw

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,28 +1,42 @@
+import axios from 'axios';
+const API_KEY = "43193709-9f2e77a8cd9049868dd3fabee"
+export async function serviceImage(image, page = "1") {
 
-export function serviceImage(image) {
-  const BASE_URL = "https://pixabay.com/api/";
-  const API_KEY = "43193709-9f2e77a8cd9049868dd3fabee"
-  const params = new URLSearchParams({
-    key: API_KEY,
-    q: image,
-    image_type: 'photo',
-    orientation: 'horizontal',
-    safesearch: 'true'
-
-  })
-
-  return fetch(`${BASE_URL}?${params}`).then(response => {
-      if (!response.ok) {
-        throw new Error(response.statusText);
-      }
-      return response.json();
-  })
-    .catch(error => {
-      
-      throw error
-      
+    const parameters = {
+      key: API_KEY,
+      q: image,
+      image_type: 'photo',
+      orientation: 'horizontal',
+      safesearch: 'true',
+      page: page,
+      per_page: 15
+    };
+ 
+  try {
+    const response = await axios.get("https://pixabay.com/api/", {
+      params: parameters
     });
+ 
+  
+    if (response.status !== 200) {
+      throw new Error(response.statusText);
+    }
+    return response.data;
+  }
+  catch (error) {
+    throw error;
+    
+  }
 }
+  
+
+
+
+
+
+
+  
+
 
 
 
--- src/main.js
+++ src/main.js
@@ -12,19 +12,35 @@ import { serviceImage } from "./js/pixabay-api";
 
 const searchImage = document.querySelector('.search-form');
 const galleryList = document.querySelector('.gallery');
-const button = document.querySelector('button');
+const button = document.querySelector('.search-btn');
 const loader = document.querySelector('.loader');
+const loadBtn = document.querySelector('.load-more')
+loadBtn.addEventListener("click", loadMore)
 
 button.addEventListener("click", handleSearch);
 searchImage.addEventListener("submit", handleSearch);
-
+let page = 1; 
 function handleSearch(evt) {
+  
   evt.preventDefault();
   const imageInput = document.querySelector(".search-form input");
   const imageValue = imageInput.value.trim();
+  if (imageValue === '') {
+      iziToast.show({
+          message: "Please enter a search query",
+          messageColor: ' #fff',
+          backgroundColor: '#ef4040',
+          position: 'topCenter',
+          messageSize: '16px',
+          messageLineHeight: '150%',
+          iconColor:'white'
+        });
+        return; 
+    }
 
   loader.style.display = 'block';
   galleryList.innerHTML = '';
+ 
 
 
   serviceImage(imageValue)
@@ -45,12 +61,18 @@ function handleSearch(evt) {
     captionDelay: 250,
     captionsData: 'alt',
     captionPosition: 'bottom',
-    
-
 };
 
 const lightbox = new SimpleLightbox('.gallery a', lightboxOptions);
-lightbox.refresh();
+        lightbox.refresh();
+       
+const perPage = 15;
+const totalPages = Math.ceil(data.total / perPage);
+
+
+if (page < totalPages) { 
+  loadBtn.classList.replace("load-hidden", "load-more");
+}   
       }
     })
       .catch(error => {
@@ -66,11 +88,33 @@ lightbox.refresh();
       });
     })
       .finally(() => {
-    
       loader.style.display = 'none';
       searchImage.reset();
     });
 }
 
+async function loadMore() {
 
+  
+  try {
+    page += 1;
+    const data = await serviceImage(page);
+    if (data && data.hits && data.hits.length > 0) {
+      galleryList.insertAdjacentHTML ('beforeend', createMarkup(data.hits));
+ console.log("Page:", page);
+    } else {
+      loadBtn.style.display = "none";
+    }
+  }
+  catch(error){}
+}
+
+function updateSearch() {
+   
+    page = 1;
+    galleryList.innerHTML = '';
+    loader.style.display = 'none';
+    
+    handleSearch();
+}
 
